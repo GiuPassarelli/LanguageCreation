f643646a9d0327eab333ccd7957a644bda89aa5a
GiuPassarelli <gb_pa@DESKTOP-R9OQ8K6.localdomain>
2020-12-07 00:21:10 -0300

    compilador

---
+++ Compiler/compiler.py
@@ -0,0 +1,408 @@
+#!/usr/bin/python
+ # pylint: disable=unused-wildcard-import, method-hidden
+
+import re
+import sys
+from tokenizer import Tokenizer
+from nodes import *
+
+class PrePro:
+    @staticmethod
+    def filter(code):
+        clean = re.sub(r'#=[\D\d]*?=#', '', code)
+        return clean
+
+class Parser:
+    @staticmethod
+    def parseBlock(isFunction):
+        bockTypes = ["EOF", "CLOSE_KEY", "ELSEIF", "ELSE"]
+        tokens = parser.tokens
+        StatNode = Statements(None)
+        while(tokens.actual.type not in bockTypes):
+            if(tokens.actual.type == "FUNCTION"):
+                tokens.selectNext()
+                type_ = tokens.actual.type
+                if(type_ != "IDENTIFIER"):
+                    raise Exception("Identifier vem depois de function")
+                
+                funcNode = FuncDec(tokens.actual.value)
+
+                tokens.selectNext()
+                type_ = tokens.actual.type
+                if(type_ != "OPEN_PAR"):
+                    raise Exception("function sem parentesis")
+                tokens.selectNext()
+                type_ = tokens.actual.type
+                
+                while(type_ == "IDENTIFIER"):
+                    variable_name = tokens.actual.value
+                    
+                    VarNode = Definition([variable_name, tokens.actual.value])
+                    funcNode.children.append(VarNode)
+
+                    tokens.selectNext()
+                    type_ = tokens.actual.type
+                    if(type_ == "COMMA"):
+                        tokens.selectNext()
+                        type_ = tokens.actual.type
+                        if(type_ != "IDENTIFIER"):
+                            raise Exception("Virgula sem variavel depois")
+                    else:
+                        if(type_ == "IDENTIFIER"):
+                            raise Exception("Variaveis devem ser separadas por virgulas")
+
+                if(type_ != "CLOSE_PAR"):
+                    raise Exception("Parentesis não é fechado")
+                tokens.selectNext()
+                if(tokens.actual.type != "OPEN_KEY"):
+                    raise Exception("Abra chaves na funcao")
+                tokens.selectNext()
+                type_ = tokens.actual.type
+                if(type_ != "ENTER"):
+                    raise Exception("Pule linha depois da declaracao")
+                tokens.selectNext()
+                type_ = tokens.actual.type
+
+                StNode = Statements(None)
+                while(tokens.actual.type != "CLOSE_KEY"):
+                    StNode.children.append(parser.parseCommand(True))
+
+                isFunction = False
+
+                funcNode.children.append(StNode)
+                
+                tokens.selectNext()
+                type_ = tokens.actual.type
+                if(type_ != "ENTER"):
+                    raise Exception("Pule linha apos o end")
+                
+                result = funcNode
+
+            else:
+                result = parser.parseCommand(isFunction)
+            StatNode.children.append(result)
+        return StatNode
+
+    @staticmethod
+    def parseCommand(isFunction):
+        tokens = parser.tokens
+        type_ = tokens.actual.type
+        result = NoOp(None)
+
+        if(type_ == "IDENTIFIER"):
+            identifier_value = tokens.actual.value
+            tokens.selectNext()
+            type_ = tokens.actual.type
+
+            if(type_ == "OPEN_PAR"):
+                fCallNode = FuncCall(identifier_value)
+                tokens.selectNext()
+                params = False
+                if(tokens.actual.type != "CLOSE_PAR"):
+                    params = True
+                while(params):
+                    params = False
+                    fCallNode.children.append(parser.parseRelExpression())
+                    if(tokens.actual.type != "CLOSE_PAR"):
+                        params = True
+                        if(tokens.actual.type != "COMMA"):
+                            raise Exception("Separe variaveis com virgula")
+                        tokens.selectNext()
+                tokens.selectNext()
+                result = fCallNode
+
+            elif(type_ == "IGUAL"):
+                tokens.selectNext()
+                type_ = tokens.actual.type
+
+                VarNode = IndentifierNode(identifier_value)
+                EqNode = Assignment(None)
+                EqNode.children.append(VarNode)
+
+                if(type_ == "INPUT"):
+                    readNode = InputNode(None)
+                    tokens.selectNext()
+                    type1 = tokens.actual.type
+                    tokens.selectNext()
+                    type2 = tokens.actual.type
+                    tokens.selectNext()
+                    if(type1 != "OPEN_PAR" or type2 != "CLOSE_PAR"):
+                        raise Exception("readline nao seguido de ()")
+                    EqNode.children.append(readNode)
+                else:
+                    EqNode.children.append(parser.parseRelExpression())
+                
+                result = EqNode
+            
+            else:
+                raise Exception("Variável solta no código")
+            
+            if(tokens.actual.type != "UNTIL"):
+                if(tokens.actual.type != "SEMI_COLLON"):
+                    raise Exception("Ponto e virgula")
+                tokens.selectNext()
+                
+        elif(type_ == "PRINT"):
+            PNode = PrintNode(None)
+
+            tokens.selectNext()
+            type_ = tokens.actual.type
+
+            if(type_ != "OPEN_PAR"):
+                raise Exception("Print deve ser seguido de parêntesis")
+
+            tokens.selectNext()
+            PNode.children.append(parser.parseRelExpression())
+            result = PNode
+
+            type_ = tokens.actual.type
+            if(type_ != "CLOSE_PAR"):
+                raise Exception("Parêntesis do print não fechado")
+
+            tokens.selectNext()
+            if(tokens.actual.type != "SEMI_COLLON"):
+                raise Exception("Ponto e virgula")
+            tokens.selectNext()
+        
+        elif(type_ == "FOR"):
+            forNode = ForNode(None)
+
+            tokens.selectNext()
+            forNode.children.append(parser.parseCommand(isFunction))
+
+            forNode.children.append(parser.parseExpression())
+
+            #tokens.selectNext()
+            if(tokens.actual.type != "OPEN_KEY"):
+                raise Exception("Abra chaves no durante")
+            tokens.selectNext()
+
+            if(tokens.actual.type != "ENTER"):
+                raise Exception("Pule linha depois do enquanto")
+
+            tokens.selectNext()
+            forNode.children.append(parser.parseBlock(isFunction))
+
+            result = forNode
+
+            type_ = tokens.actual.type
+            if(type_ != "CLOSE_KEY"):
+                raise Exception("Coloque END depois de um enquanto")
+            
+            tokens.selectNext()
+        
+        elif(type_ == "WHILE"):
+            whileNode = WhileNode(None)
+            
+            tokens.selectNext()
+            whileNode.children.append(parser.parseRelExpression())
+
+            if(tokens.actual.type != "OPEN_KEY"):
+                raise Exception("Abra chaves no enquanto")
+            tokens.selectNext()
+
+            if(tokens.actual.type != "ENTER"):
+                raise Exception("Pule linha depois do enquanto")
+
+            tokens.selectNext()
+            whileNode.children.append(parser.parseBlock(isFunction))
+
+            result = whileNode
+
+            type_ = tokens.actual.type
+            if(type_ != "CLOSE_KEY"):
+                raise Exception("Coloque END depois de um enquanto")
+            
+            tokens.selectNext()
+
+        elif(type_ == "IF"):
+            originalNode = IfNode(None)
+            ifNode = originalNode
+            while(type_ == "IF" or type_ == "ELSEIF"):
+                
+                tokens.selectNext()
+                ifNode.children.append(parser.parseRelExpression())
+
+                if(tokens.actual.type != "OPEN_KEY"):
+                    raise Exception("Abra chves no if")
+                tokens.selectNext()
+
+                if(tokens.actual.type != "ENTER"):
+                    raise Exception("Pule linha depois do if")
+
+                tokens.selectNext()
+                ifNode.children.append(parser.parseBlock(isFunction))
+
+                if(tokens.actual.type != "CLOSE_KEY"):
+                    raise Exception("Feche chves no if")
+                tokens.selectNext()
+
+                if(tokens.actual.type != "ENTER"):
+                    raise Exception("Pule linhas")
+                tokens.selectNext()
+
+                type_ = tokens.actual.type
+
+                if(type_ == "ELSEIF"):
+                    elseifNode = IfNode(None)
+                    ifNode.children.append(elseifNode)
+                    ifNode = elseifNode
+                
+                elif(type_ == "ELSE"):
+                    elseNode = ElseNode(None)
+                    ifNode.children.append(elseNode)
+                    tokens.selectNext()
+                    if(tokens.actual.type != "OPEN_KEY"):
+                        raise Exception("Abra chves no if")
+                    tokens.selectNext()
+                    type1 = tokens.actual.type
+                    if(type1 != "ENTER"):
+                        raise Exception("Pule linha depois do if")
+
+                    tokens.selectNext()
+                    elseNode.children.append(parser.parseBlock(isFunction))
+
+            type_ = tokens.actual.type
+            if(type_ != "CLOSE_KEY"):
+                raise Exception("Coloque END depois de um if")
+            
+            result = originalNode
+            tokens.selectNext()
+        
+        elif(type_ == "RETURN" and isFunction):
+            returnNode = ReturnNode(None)
+            tokens.selectNext()
+            returnNode.children.append(parser.parseRelExpression())
+            result = returnNode
+            if(tokens.actual.type != "SEMI_COLLON"):
+                raise Exception("Ponto e virgula")
+            tokens.selectNext()
+
+        elif(type_ == "RETURN" and not(isFunction)):
+            raise Exception("Return so funciona dentro de funcoes")
+
+        type_ = tokens.actual.type
+        if(type_ == "ENTER" or type_ == "UNTIL"):
+            tokens.selectNext()
+            return (result)
+
+        raise Exception("Dê enter após operações")
+
+    @staticmethod
+    def parseRelExpression():
+        relExpTypes = ["MAIOR", "MENOR", "COMPARACAO"]
+        tokens = parser.tokens
+        result = parser.parseExpression()
+        type_ = tokens.actual.type
+        while(type_ in relExpTypes):
+            BinNode = BinOp(tokens.actual.value)
+            BinNode.children.append(result)
+            tokens.selectNext()
+
+            BinNode.children.append(parser.parseExpression())
+            result = BinNode
+            type_ = tokens.actual.type
+        return(result)
+
+    @staticmethod
+    def parseExpression():
+        expressionTypes = ["PLUS", "MINUS", "OR"]
+        tokens = parser.tokens
+        result = parser.parseTerm()
+        type_ = tokens.actual.type
+        while(type_ in expressionTypes):
+            BinNode = BinOp(tokens.actual.value)
+            BinNode.children.append(result)
+            tokens.selectNext()
+
+            BinNode.children.append(parser.parseTerm())
+            result = BinNode
+            type_ = tokens.actual.type
+        return(result)
+
+    @staticmethod
+    def parseTerm():
+        termTypes = ["MULTIPLY", "DIVIDE", "AND"]
+        tokens = parser.tokens
+        result = parser.parseFactor()
+        type_ = tokens.actual.type
+        while(type_ in termTypes):
+            BinNode = BinOp(tokens.actual.value)
+            BinNode.children.append(result)
+            tokens.selectNext()
+
+            BinNode.children.append(parser.parseFactor())
+            result = BinNode
+            type_ = tokens.actual.type
+        return(result)
+
+    @staticmethod
+    def parseFactor():
+        factorTypes = ["PLUS", "MINUS", "NOT"]
+        tokens = parser.tokens
+        type_ = tokens.actual.type
+        if(type_ == "INT"):
+            result = IntVal(tokens.actual.value)
+            tokens.selectNext()
+        elif(type_ in factorTypes):
+            result = UnOp(tokens.actual.value)
+            tokens.selectNext()
+            result_factor = parser.parseFactor()
+            result.children.append(result_factor)
+        elif(type_ == "OPEN_PAR"):
+            tokens.selectNext()
+            result = parser.parseRelExpression()
+            if(tokens.actual.type != "CLOSE_PAR"):
+                raise Exception("Parentesis não fechado")
+            tokens.selectNext()
+        elif(type_ == "IDENTIFIER"):
+            valor = tokens.actual.value
+            tokens.selectNext()
+            if(tokens.actual.type == "OPEN_PAR"):
+                fCallNode = FuncCall(valor)
+                tokens.selectNext()
+                params = False
+                if(tokens.actual.type != "CLOSE_PAR"):
+                    params = True
+                while(params):
+                    params = False
+                    fCallNode.children.append(parser.parseRelExpression())
+                    if(tokens.actual.type != "CLOSE_PAR"):
+                        params = True
+                        if(tokens.actual.type != "COMMA"):
+                            raise Exception("Separe variaveis com virgula")
+                        tokens.selectNext()
+                tokens.selectNext()
+                result = fCallNode
+            else:
+                INode = IndentifierNode(valor)
+                result = INode
+        elif(type_ == "TRUE" or type_ == "FALSE"):
+            result = BoolVal(tokens.actual.value)
+            tokens.selectNext()
+        elif(type_ == "STRING"):
+            result = StringVal(tokens.actual.value)
+            tokens.selectNext()
+        else:
+            raise Exception("Deu errado")
+        return result
+
+    @staticmethod
+    def run(code):
+        clean = PrePro.filter(code)
+        parser.tokens = Tokenizer(clean)
+        parser.tokens.selectNext()
+        result = parser.parseBlock(False)
+        if(parser.tokens.actual.type != "EOF"):
+            raise Exception("Não terminou direito")
+        return(result)
+
+f = open(sys.argv[1], "r")
+entrada = []
+entrada.append(str(f.read()))
+
+parser = Parser()
+result = parser.run(entrada[0])
+result.Evaluate(0)
+
+#SO PULA 1 QUANDO EH BOLINHA!!
---
+++ Compiler/tokenizer.py
@@ -0,0 +1,91 @@
+class Token:
+    def __init__(self, _type, value):
+        self.type = _type
+        self.value = value
+
+class Tokenizer:
+    def __init__(self, origin):
+        self.origin = origin
+        self.position = 0
+        self.actual = None
+        self.value_dict = {"+": "PLUS", "-": "MINUS", "*": "MULTIPLY", "/": "DIVIDE", "(": "OPEN_PAR", 
+                            ")": "CLOSE_PAR", "\n": "ENTER", "!": "NOT", ";": "SEMI_COLLON",
+                            ",": "COMMA", "{": "OPEN_KEY", "}": "CLOSE_KEY"}
+        self.reserved_dict = {"imprime": "PRINT", "se": "IF", "senao": "ELSE", 
+                            "enquanto": "WHILE", "true": "TRUE", "false": "FALSE", "funcao": "FUNCTION", 
+                            "retorna": "RETURN", "durante": "FOR", "ate": "UNTIL"}
+        self.double_define = {"&": "AND", "|": "OR", "=": "COMPARACAO", ":": "DEFINICAO"}
+
+    def selectNext(self):
+        pos = self.position
+        if(len(self.origin)>pos):
+            carac = self.origin[pos]
+
+            if(carac.isdigit()):
+                while(len(self.origin)>(pos+1) and (self.origin[pos+1]).isdigit()):
+                    pos += 1
+                    carac += self.origin[pos]
+                self.actual = Token("INT", int(carac))
+            
+            elif(carac.isalpha()):
+                while(len(self.origin)>(pos+1) and ((self.origin[pos+1]).isalpha() or (self.origin[pos+1]).isdigit() or (self.origin[pos+1]) == "_")):
+                    carac += self.origin[pos+1]
+                    pos += 1
+                if(carac in self.reserved_dict):
+                    self.actual = Token(self.reserved_dict[carac], carac)
+                elif(carac=="maiorQue"):
+                    self.actual = Token("MAIOR", ">")
+                elif(carac=="menorQue"):
+                    self.actual = Token("MENOR", "<")
+                else:
+                    self.actual = Token("IDENTIFIER", carac)
+            
+            elif(carac == '"'):
+                value = ""
+                pos += 1
+                carac = self.origin[pos]
+                while(carac != '"'):
+                    if(carac == "EOF"):
+                        raise Exception("String não fechada")
+                    value += carac
+                    pos += 1
+                    carac = self.origin[pos]
+                self.actual = Token("STRING", value)
+
+            elif(carac in self.double_define and self.origin[pos+1]==carac):
+                pos+=1
+                self.actual = Token(self.double_define[carac], carac+carac)
+            
+            elif(carac == "-" and self.origin[pos+1] == ">"):
+                pos+=1
+                self.actual = Token("IGUAL", "->")
+
+            elif(carac in self.value_dict):
+                self.actual = Token(self.value_dict[carac], carac)
+
+            elif(carac == " "):
+                if(self.actual is not None):
+                    type1 = self.actual.type
+                else:
+                    type1 = "None"
+
+                while(carac == " "):
+                    self.position += 1
+                    pos = self.selectNext() - 1
+                    if(self.actual.type!="EOF"):
+                        carac = self.origin[pos]
+                    else:
+                        carac = "EOF"
+                type2 = self.actual.type
+                if((type2 == "INT" or type2 == "IDENTIFIER") and (type1 == "INT" or type1 == "IDENTIFIER")):
+                    raise Exception("Números sem operação entre eles")
+            
+            else:
+                raise Exception("Caractér não reconhecido")
+
+        else:
+            self.actual = Token("EOF", "")
+
+        pos += 1
+        self.position = pos
+        return self.position
